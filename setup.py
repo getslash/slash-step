74d0669b0a25c805799ef8a6bfc65d07c64a50c8
Omer Gertel <omerg@infinidat.com>
2013-07-25 13:03:57 +0300

    fix dependancy

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ with open(_in_same_dir("slash_step", "__version__.py")) as version_file:
     exec(version_file.read())  # pylint: disable=W0122
 
 install_requires = [
-    "slash",
+    "slash>=0.0.3",
 ]
 
 setup(name="slash_step",
-      classifiers = [
+      classifiers=[
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3.3",
@@ -27,10 +27,4 @@ setup(name="slash_step",
       version=__version__, # pylint: disable=E0602
       packages=find_packages(exclude=["tests"]),
       install_requires=install_requires,
-      entry_points = dict(
-          console_scripts = [
-              "slash  = slash.frontend.main:main_entry_point",
-              ]
-          ),
-
       )
