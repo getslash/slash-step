7f3230842ef27cf0e0a85e5c26d5cb8c76767696
Omer Gertel <omerg@infinidat.com>
2014-12-02 16:27:41 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open(_in_same_dir("slash_step", "__version__.py")) as version_file:
     exec(version_file.read())  # pylint: disable=W0122
 
 install_requires = [
-    "slash>=0.6.0",
+    "slash>=0.7.0",
 ]
 
 setup(name="slash_step",
       classifiers=[
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
           ],
       description="A more granular sub STEP for slash tests",
       license="BSD",
